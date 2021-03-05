b65fe8b0ab44493d4d4352b54837fb8a1470aeac
jorgecastilloprz <jorge.castillo.prz@gmail.com>
2021-03-05 09:50:30 +0100

    Removes stroke cap.

--- app/src/main/java/com/example/androiddevchallenge/CountdownArc.kt
+++ app/src/main/java/com/example/androiddevchallenge/CountdownArc.kt
@@ -22,8 +22,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,7 +29,6 @@ import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.CountdownTimerTheme
 import com.example.androiddevchallenge.ui.theme.primary
 import com.example.androiddevchallenge.ui.theme.primaryLight
-import kotlin.math.atan
 
 @Composable
 fun CountdownArc(totalTimeSeconds: Long, remainingTimeSeconds: Long) {
@@ -51,23 +48,15 @@ fun CountdownArc(totalTimeSeconds: Long, remainingTimeSeconds: Long) {
                 style = Stroke(width = arcWidth)
             )
 
-            val capOffset = degreesRequiredForStrokeCap(arcWidth * 1.1f)
-
             drawArc(
                 brush = Brush.sweepGradient(listOf(primaryLight, primary)),
-                startAngle = capOffset,
-                sweepAngle = remainingPercent * 360f - capOffset,
+                startAngle = 0f,
+                sweepAngle = remainingPercent * 360f,
                 useCenter = false,
-                style = Stroke(width = arcWidth * 1.1f, cap = StrokeCap.Round)
+                style = Stroke(width = arcWidth * 1.1f)
             )
         }
     )
-}
-
-private fun DrawScope.degreesRequiredForStrokeCap(strokeWidth: Float): Float {
-    val adjacent = (size.minDimension - strokeWidth) / 2
-    val opposite = strokeWidth / 2.0
-    return Math.toDegrees(atan(opposite / adjacent)).toFloat()
 }
 
 @Preview
